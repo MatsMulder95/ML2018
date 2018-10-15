fb6f90f55c2409f452e0eb6fb89379d34b3173cb
gijswissing <gijs.wissi@gmail.com>
2018-10-15 17:29:56 +0300

    59% version

--- Code/FilterData.py
+++ Code/FilterData.py
@@ -9,7 +9,9 @@ import random
 ##importing data
 
 data = pd.read_csv('data/train_data.csv',header= None)
+test = pd.read_csv('data/test_data.csv',header=None)
 labels = pd.read_csv('data/train_labels.csv',header= None)
+data.iloc[np.where(data.values==1000000)]=1E-6
 
 ##normalizing data
 min_max_scaler = preprocessing.MinMaxScaler()
@@ -27,14 +29,17 @@ for i in range(1,25):
     for s in range(7):
         dataScaled = dataScaled.rename(columns = {(i-1)+s*24:str(i)+'_Band_'+ statistic[s]})
         data = data.rename(columns = {(i-1)+s*24:str(i)+'_Band_'+ statistic[s]})
+        test = test.rename(columns = {(i-1)+s*24:str(i)+'_Band_'+ statistic[s]})
 for i in range(1,13):
     for s in range(4):
         dataScaled = dataScaled.rename(columns = {168 + (i-1)+s*12:str(i)+'_Class_'+measurements[s]})
         data = data.rename(columns = {168 + (i-1)+s*12:str(i)+'_Class_'+measurements[s]})
+        test = test.rename(columns = {168 + (i-1)+s*12:str(i)+'_Class_'+measurements[s]})
 for i in range(1,13):
     for s in range(4):
         dataScaled = dataScaled.rename(columns = {216 + (i-1)+s*12:str(i)+'_Coefficients_'+measurements[s]})
-        data = data.rename(columns = {216 + (i-1)+s*12:str(i)+'_Coefficients_'+measurements[s]})        
+        data = data.rename(columns = {216 + (i-1)+s*12:str(i)+'_Coefficients_'+measurements[s]})
+        test = test.rename(columns = {216 + (i-1)+s*12:str(i)+'_Coefficients_'+measurements[s]})        
 
 ##adding label data
 data['label'] = labels
@@ -43,6 +48,7 @@ dataScaled['label'] = labels
 ##example use of filter based on new column naming
 dataMeans = dataScaled.filter(regex='mean')
 data.to_csv('data/filtered/ordered_data.csv')
+test.to_csv('data/filtered/ordered_test_data.csv')
 genreLength = []
 indexSelect = []
 for i in range(1,11):
@@ -53,7 +59,21 @@ for genre in range(1,11):
     genreSelect = random.sample(genreIndex,lenMin)
     indexSelect.extend(genreSelect)
 filteredData = data.loc[indexSelect]
-filteredData.to_csv('data/filtered/filtered_data.csv')
+columnList = []
+columnList.extend(data.filter(regex='mean').columns.tolist())
+columnList.extend(data.filter(regex='variance').columns.tolist())
+columnList.extend(data.filter(regex='kurtosis').columns.tolist())
+columnList.extend(data.filter(regex='skewness').columns.tolist())
+columnList.extend(data.filter(regex='stddev').columns.tolist())
+testFeaturesSelection = test[columnList]
+testFeaturesSelection.to_csv('data/filtered/selected_test.csv')
+columnList.extend(['label'])
+featureSelection = data[columnList]
+featureSelection.to_csv('data/filtered/selected_data.csv')
+
+
+##filteredData = data.loc[indexSelect]
+##filteredData.to_csv('data/filtered/filtered_data.csv')
 ##means = []
 ##variances = []
 ##for label in range(1,11):
@@ -92,22 +112,21 @@ filteredData.to_csv('data/filtered/filtered_data.csv')
 ##plt.legend()
 ##plt.show()
 
-means = []
-stddev = []
-data.iloc[np.where(data.values==1000000)]=1E-6
-for label in range(1,11):
-    genre = data.loc[data.label==label]
-    for Coefficients in range(1,13):
-        averageMean = genre[str(Coefficients)+'_Coefficients_mean'].mean()
-        if averageMean==1000000:
-            averageMean=1E-6
-        averageStddev = np.sqrt((np.sum(genre[str(Coefficients)+'_Coefficients_stddev']**2)+np.sum((genre[str(Coefficients)+'_Coefficients_mean']-averageMean)**2))/data.shape[0])
-        means.append(averageMean)
-        stddev.append(averageStddev)
-    plt.errorbar(range(1,13),means[12*(label-1):12*label],stddev[12*(label-1):12*label],label = 'genre'+ str(label))
-plt.title('MFCCs genre average data')
-plt.legend()
-plt.show()
+##means = []
+##stddev = []
+##for label in range(1,11):
+##    genre = data.loc[data.label==label]
+##    for Coefficients in range(1,13):
+##        averageMean = genre[str(Coefficients)+'_Coefficients_mean'].mean()
+##        if averageMean==1000000:
+##            averageMean=1E-6
+##        averageStddev = np.sqrt((np.sum(genre[str(Coefficients)+'_Coefficients_stddev']**2)+np.sum((genre[str(Coefficients)+'_Coefficients_mean']-averageMean)**2))/data.shape[0])
+##        means.append(averageMean)
+##        stddev.append(averageStddev)
+##    plt.errorbar(range(1,13),means[12*(label-1):12*label],stddev[12*(label-1):12*label],label = 'genre'+ str(label))
+##plt.title('MFCCs genre average data')
+##plt.legend()
+##plt.show()
 
 ####creating a plot of means for each band *option for one color per genre and 1 vs all possible
 ##for label in range(1,11):#this is for iterating through all labels
--- Code/Main.py
+++ Code/Main.py
@@ -1,17 +1,47 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import preprocessing, neighbors, datasets
+from matplotlib.colors import ListedColormap
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+from sklearn import svm
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+import csv
+
 from ClassifyData import *
-from CalculateResult import *
 
-# Filter Data
+# importing data
+train_data = pd.read_csv('data/filtered/selected_data.csv',index_col=0).iloc[:,:-1]
+train_labels = pd.read_csv('data/filtered/selected_data.csv',index_col=0).iloc[:,-1]
+test_data = pd.read_csv('data/filtered/selected_test.csv', index_col=0)
+
+print(train_data.shape)
+print(train_labels.shape)
+print(test_data.shape)
+
+#train_data['label'] = train_labels
 
 
-# Visualize Data
+# Set input
+X_train = train_data   # Input train data
+y_train = train_labels    # Input train labels
+X_validate = test_data  # Input validate data
 
 
-# Classify Data
-predict = classify('logistic')
->>>>>>> 8f844c2071385d6ead30db8cb96cca09e46acfe4
+#Logistic BEST!!!
+predict = LogisticRegression(random_state=0, solver='lbfgs', multi_class='ovr', max_iter=2000).fit(X_train, y_train).predict(X_validate)
+predict1 = LogisticRegression(random_state=1, solver='lbfgs', multi_class='ovr', max_iter=2000).fit(X_train, y_train).predict(X_validate)
 
-# Calculate outcome
-result = getResult(predict)
 
-# Export to CSV
+
+#Write to CSV
+with open("data/output.csv", "w", newline='') as myfile:
+    wr = csv.writer(myfile)
+    wr.writerow(["Sample_id","Sample_label"])
+    for i in range(0,X_validate.shape[0]):
+        wr.writerow([i+1,str(predict[i].item())])
