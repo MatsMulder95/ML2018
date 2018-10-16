196343f6adb23b29c8972ef1c7ce695c0fe33265
MatsMulder95 <mats.mulder@aatlo.fi>
2018-10-16 10:51:09 +0300

    Set up final structure

--- Code/ClassifyData.py
+++ Code/ClassifyData.py
@@ -6,14 +6,31 @@ from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
 
 
-def classify(type='logistic'):
-    # Import filtered data en labels
-    trainData = pd.read_csv('data/filtered/train_data.csv', header=None)
-    trainLabels = pd.read_csv('data/filtered/train_labels.csv', header=None)[0]
-    testData = pd.read_csv('data/filtered/test_data.csv', header=None)
+def classify(trainData, trainLabels, testData, type='logistic',run='non_local'):
 
-    # classify using type
+    # Split data on local run
+    if run == 'local':
+        sub_index = 3000
+        train_data_sub1 = trainData.sample(sub_index)
+        train_labels_sub1 = trainLabels.iloc[train_data_sub1.index]
+        train_data_sub2 = trainData.iloc[~trainData.index.isin(train_data_sub1.index)]
+        train_labels_sub2 = trainLabels.iloc[train_data_sub2.index]
+
+        trainData = train_data_sub1.reset_index(drop=True)
+        trainLabels = train_labels_sub1.reset_index(drop=True)
+        testData = train_data_sub2.reset_index(drop=True)
+        testLabels = train_labels_sub2.reset_index(drop=True)
+
+    # Classify using type
     if type == 'logistic':
         predict = LogisticRegression(random_state=0, solver='lbfgs', multi_class='ovr', max_iter=2000).fit(trainData,trainLabels).predict(testData)
+
+    # Console print result
+    if run == 'local':
+        result = 0
+        for i in range(0, testLabels.shape[0]):
+            if predict[i] == testLabels[i]:
+                result = result + 1
+        print(result / testLabels.shape[0])
 
     return predict
--- Code/Main.py
+++ Code/Main.py
@@ -1,54 +1,15 @@
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import preprocessing, neighbors, datasets
-from matplotlib.colors import ListedColormap
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC
-from sklearn import svm
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
-import csv
-
+from csvWrite import *
 from ClassifyData import *
 
-# importing data
-train_data = pd.read_csv('data/filtered/selected_data.csv',index_col=0).iloc[:,:-1]
-train_labels = pd.read_csv('data/filtered/selected_data.csv',index_col=0).iloc[:,-1]
-test_data = pd.read_csv('data/filtered/selected_test.csv', index_col=0)
+# Import en Filter data
+trainData = pd.read_csv('data/filtered/selected_data.csv',index_col=0).iloc[:,:-1]
+trainLabels = pd.read_csv('data/filtered/selected_data.csv',index_col=0).iloc[:,-1]
+testData = pd.read_csv('data/filtered/selected_test.csv', index_col=0)
 
-print(train_data.shape)
-print(train_labels.shape)
-print(test_data.shape)
+# Train data
+predict = classify(trainData, trainLabels, testData, 'logistic', 'local')
 
-#train_data['label'] = train_labels
+# Visualization
 
-###Visualize data
-
-###Train Classifier
-
-# Set input
-X_train = train_data   # Input train data
-y_train = train_labels    # Input train labels
-X_validate = test_data  # Input validate data
-
-
-#Logistic BEST!!!
-predict = LogisticRegression(random_state=0, solver='lbfgs', multi_class='ovr', max_iter=2000).fit(X_train, y_train).predict(X_validate)
-predict1 = LogisticRegression(random_state=1, solver='lbfgs', multi_class='ovr', max_iter=2000).fit(X_train, y_train).predict(X_validate)
-
-
-
-#Write to CSV
-with open("data/output.csv", "w", newline='') as myfile:
-    wr = csv.writer(myfile)
-    wr.writerow(["Sample_id","Sample_label"])
-    for i in range(0,X_validate.shape[0]):
-        wr.writerow([i+1,str(predict[i].item())])
-
-###Visualize output
-
-        
+# Write to csv
+writetocsv(predict)
